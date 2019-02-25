562cae556a90ef98a78ad8676a018224a1f3de16
Xen <froehlich.grg@gmail.com>
2019-02-25 17:13:02 +0100

    Домашняя работа №2

---
+++ lesson-02-dz/src/App.js
@@ -0,0 +1,33 @@
+const externalfile1 = {
+    width: "99%",
+    margin: "30px auto",
+    backgroundColor: "#CC0000",
+    minHeight: "100px",
+    boxSizing: "border-box",
+    color: "#FFFFFF",
+    padding: "2px 6px 0 2px"
+};
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import External1 from './externalfile1';
+import External2 from './externalfile2';
+import Article from './externalfile3';
+import {articles} from "./texts";
+
+class App extends React.Component{
+    render() {
+        return <div style = {externalfile1}>
+            <h2>Test text</h2>
+            <br />
+            <span>Text text</span>
+        </div>;
+    }
+}
+
+ReactDOM.render(<External1/>, document.getElementById('external-file-1'));
+ReactDOM.render(<External2 firstName={'Max'} lastName={'Max last name'}/>, document.getElementById('external-file-2'));
+ReactDOM.render(<Article article = {articles[2]}/>, document.getElementById('external-file-3'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+
---
+++ lesson-02-dz/src/externalfile1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function external1() {
+    return(
+        <div>
+            Что-то из нового файла 1 {}
+        </div>
+    )
+}
